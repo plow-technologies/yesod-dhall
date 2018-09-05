133069c57f2b1ca60e399c5b9ba135e6edfa9cb8
Edward Amsden <edward.amsden@plowtech.net>
2018-09-05 15:03:09 -0400

    Switch to prettyLayout for pretty-printed Dhall

--- src/Yesod/Dhall.hs
+++ src/Yesod/Dhall.hs
@@ -43,7 +43,7 @@ instance Inject a => ToContent (DhallResponse a) where
   toContent (DhallResponse a) =
       toContent
     $ renderStrict
-    $ layoutCompact
+    $ layoutPretty defaultLayoutOptions
     $ prettyExpr
     $ embed inject a
 
